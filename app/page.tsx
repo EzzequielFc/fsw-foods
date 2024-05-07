614e7ef43c364224711addd281d34133a72aff77
EzzequielFc <contato.ezequielf@gmail.com>
2024-05-07 09:33:05 -0300

    refactor: ensure product list receive products as props

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,25 @@ import Search from "./components/search";
 import ProductList from "./components/product-list";
 import { Button } from "./components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
+import { db } from "./lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -34,11 +51,14 @@ export default function Home() {
       <div className="pt-6 space-y-2">
         <div className="px-5 flex items-center justify-between">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button variant="ghost" className="text-primary p-0 hover:bg-transparent" >
+          <Button
+            variant="ghost"
+            className="text-primary p-0 hover:bg-transparent"
+          >
             Ver todos <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
       </div>
     </>
   );
