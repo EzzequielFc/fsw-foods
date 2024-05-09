b2cd0a597effe6ee8f22ea6fd2cdc5c9a4eb3d0c
EzzequielFc <contato.ezequielf@gmail.com>
2024-05-09 15:45:39 -0300

    feat: add createdAt propetry

--- app/components/restaurant-item.tsx
+++ app/components/restaurant-item.tsx
@@ -3,6 +3,7 @@ import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../helpers/price";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
@@ -10,43 +11,51 @@ interface RestaurantItemProps {
 
 export default function RestaurantItem({ restaurant }: RestaurantItemProps) {
   return (
-    <div className="min-w-[266px] max-w-[266px] space-y-3 mb-6">
-      <div className="w-full h-[136px] relative">
-        <Image
-          src={restaurant.imageUrl}
-          fill
-          className="object-cover rounded-lg shadow-md"
-          alt={restaurant.name}
-        />
+    <Link
+      className="min-w-[266px] max-w-[266px]"
+      href={`/restaurants/${restaurant.id}`}
+    >
+      <div className="w-full space-y-3 mb-6">
+        <div className="w-full h-[136px] relative">
+          <Image
+            src={restaurant.imageUrl}
+            fill
+            className="object-cover rounded-lg shadow-md"
+            alt={restaurant.name}
+          />
 
-        <div className="absolute gap-1 top-2 left-2 bg-primary px-2 py-[2px] rounded-full flex items-center text-white bg-white">
-          <StarIcon size={12} className="fill-yellow-500 text-yellow-400" />
-          <span className="font-semibold text-xs text-black">5,0</span>
+          <div className="absolute gap-1 top-2 left-2 bg-primary px-2 py-[2px] rounded-full flex items-center text-white bg-white">
+            <StarIcon size={12} className="fill-yellow-500 text-yellow-400" />
+            <span className="font-semibold text-xs text-black">5,0</span>
+          </div>
+
+          <Button
+            size={"icon"}
+            className="absolute top-2 right-2 bg-gray-600 rounded-full w-7 h-7"
+          >
+            <HeartIcon size={16} className="fill-white" />
+          </Button>
         </div>
 
-        <Button size={"icon"} className="absolute top-2 right-2 bg-gray-600 rounded-full w-7 h-7">
-          <HeartIcon size={16} className="fill-white"/>
-        </Button>
+        <h3 className="font-semibold text-sm">{restaurant.name}</h3>
+        <div className="flex gap-3">
+          <div className="flex gap-1 items-center">
+            <BikeIcon size={12} className="text-primary" />
+            <span className="text-xs text-muted-foreground">
+              {Number(restaurant.deliveryFee) === 0
+                ? "Entrega grátis"
+                : `${formatCurrency(Number(restaurant.deliveryFee))}`}
+            </span>
+          </div>
+
+          <div className="flex gap-1 items-center">
+            <TimerIcon size={12} className="text-primary" />
+            <span className="text-xs text-muted-foreground">
+              {restaurant.deliveryTimeMinutes} min
+            </span>
+          </div>
+        </div>
       </div>
-
-      <h3 className="font-semibold text-sm">{restaurant.name}</h3>
-      <div className="flex gap-3">
-        <div className="flex gap-1 items-center">
-          <BikeIcon size={12} className="text-primary" />
-          <span className="text-xs text-muted-foreground">
-            {Number(restaurant.deliveryFee) === 0
-              ? "Entrega grátis"
-              : `${formatCurrency(Number(restaurant.deliveryFee))}`}
-          </span>
-        </div>
-
-        <div className="flex gap-1 items-center">
-          <TimerIcon size={12} className="text-primary" />
-          <span className="text-xs text-muted-foreground">
-            {restaurant.deliveryTimeMinutes} min
-          </span>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 }
--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -1,7 +1,106 @@
-interface RestaurantProps {
-    
+import { db } from "@/app/lib/prisma";
+import { notFound } from "next/navigation";
+import RestaurantImage from "./components/restaurant-image";
+import Image from "next/image";
+import { StarIcon } from "lucide-react";
+import { DeliveryInfo } from "@/app/components/delivery-info";
+import ProductList from "@/app/components/product-list";
+
+interface RestaurantPageProps {
+  params: {
+    id: string;
+  };
 }
 
-export default function Restaurant() {
-  return <h1>Olá</h1>;
+export default async function RestaurantPage({
+  params: { id },
+}: RestaurantPageProps) {
+  const restaurant = await db.restaurant.findUnique({
+    where: { id },
+    include: {
+      categories: {
+        include: {
+          products: {
+            where: {
+              restaurantId: id,
+            },
+            include: {
+              restaurant: {
+                select: {
+                  name: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      products: {
+        take: 10,
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <RestaurantImage restaurant={restaurant} />
+
+      <div className="flex justify-between items-center px-5 pt-5 relative z-50 mt-[-1.5rem] bg-white rounded-lg">
+        {/** TITULO */}
+        <div className="flex items-center gap-[0.375rem]">
+          <div className="relative h-8 w-8">
+            <Image
+              src={restaurant.imageUrl}
+              alt={restaurant.name}
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+          <h1 className="text-xl font-semibold">{restaurant.name}</h1>
+        </div>
+
+        <div className="bg-foreground gap-2 px-2 py-[2px] rounded-full flex items-center text-white bg-white">
+          <StarIcon size={12} className="fill-yellow-500 text-yellow-400" />
+          <span className="font-semibold text-xs text-white ">5,0</span>
+        </div>
+      </div>
+
+      <DeliveryInfo restaurant={restaurant} />
+
+      <div className="flex overflow-x-scroll gap-4 [&::-webkit-scrollbar]:hidden mt-3 px-5">
+        {restaurant.categories.map((category) => (
+          <div
+            key={category.id}
+            className="bg-[#F4F4F4] min-w-[167px] rounded-lg text-center"
+          >
+            <span className="text-xs text-muted-foreground">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="font-semibold mb-3">Mais Pedidos</h2>
+        <ProductList products={restaurant.products} />
+      </div>
+
+      {restaurant.categories.map((category) => (
+        <div key={category.id} className="px-5 mt-6">
+          <h2 className="font-semibold mb-3">{category.name}</h2>
+          <ProductList products={category.products} />
+        </div>
+      ))}
+    </div>
+  );
 }
